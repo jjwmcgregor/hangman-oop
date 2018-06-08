84016c80d9f3edd4e7cfeb30d4033202dc3fe9ba
Tech-A22 <tech-a22@tech-a22s-air.home>
2018-06-08 22:15:06 +0100

    made 2-[;ayer

--- js/app.js
+++ js/app.js
@@ -11,8 +11,8 @@ $(document).ready(() => {
 
   Game.$lettersContainer = $('.lettersContainer');
   Game.$inputContainer = $('.inputContainer');
-  Game.$playerWord = $('.playerWord');
-  Game.$playerSubmit = $('.playerSubmit');
+  Game.$playerWord = $('#playerWord');
+  Game.$playerSubmit = $('#playerSubmit');
   Game.$guessInput = $('#guessInput');
   Game.$guessSubmit = $('#guessSubmit');
   Game.$start = $('#start');
@@ -32,44 +32,40 @@ $(document).ready(() => {
         }
       }
       // Lose a life
-      if (!Game.wordsArr[Game.randomIndex].includes(Game.letter)) {
+      if (!Game.wordToPlay.includes(Game.letter)) {
         $('#someSpan').append(`<h4 class="wrongGuess">${ Game.letter }</h4>`);
         Game.lives--;
       }
 
       switch (Game.lives) {
         case 7:
-        $('.gallows').append('<div class="noose" />');
-        break;
+          $('.gallows').append('<div class="noose" />');
+          break;
         case 6:
-        $('.gallows').append('<div class="head" />');
-        break;
+          $('.gallows').append('<div class="head" />');
+          break;
         case 5:
-        $('.gallows').append('<div class="body" />');
-        break;
+          $('.gallows').append('<div class="body" />');
+          break;
         case 4:
-        $('.gallows').append('<div class="leftArm" />');
-        break;
+          $('.gallows').append('<div class="leftArm" />');
+          break;
         case 3:
-        $('.gallows').append('<div class="rightArm" />');
-        break;
+          $('.gallows').append('<div class="rightArm" />');
+          break;
         case 2:
-        $('.gallows').append('<div class="rightLeg" />');
-        break;
+          $('.gallows').append('<div class="rightLeg" />');
+          break;
         case 1:
-        $('.gallows').append('<div class="leftLeg" />');
-        $('.loseMsg').show();
-        $('.gallows').css('background','black');
-        $('.hangmanBoard').css('background','black');
-        $('.gallows').children().css('background','white');
-        break;
+          $('.gallows').append('<div class="leftLeg" />');
+          $('.loseMsg').show();
+          $('.gallows').css('background','black');
+          $('.hangmanBoard').css('background','black');
+          $('.gallows').children().css('background','white');
+          break;
         default:
 
       }
-      // Original lose condition
-      // if (Game.lives === 0) {
-      //   console.log('you lose');
-      // }
       Game.$guessInput.val('');
     } else {
       $('.errorMsg').show();
@@ -78,12 +74,12 @@ $(document).ready(() => {
   }
 
 
-  Game.setupWord = function(){
-    for (var i = 0; i < Game.wordsArr[Game.randomIndex].length; i++) {
-      if (Game.wordsArr[Game.randomIndex][i] === ' ') {
-        Game.$lettersContainer.append(`<div class="space">${ Game.wordsArr[Game.randomIndex][i] }</div>`)
+  Game.setupWord = function(word){
+    for (var i = 0; i < word.length; i++) {
+      if (word[i] === ' ') {
+        Game.$lettersContainer.append(`<div class="space">${ word[i] }</div>`)
       } else {
-        Game.$lettersContainer.append(`<div class="letter">${ Game.wordsArr[Game.randomIndex][i] }</div>`)
+        Game.$lettersContainer.append(`<div class="letter">${ word[i] }</div>`)
       }
     }
   }
@@ -95,23 +91,32 @@ $(document).ready(() => {
         count++;
       }
       if (count === $('.letter').length) {
-        console.log('win');
+        $('.winMsg').show();
       }
     }
     count = 0;
   }
 
-  Game.setupWord();
-
-  Game.$start.click(function(){
+  // Player 2 Word generation
+  Game.$playerSubmit.click(function(){
+    Game.wordToPlay = $('#playerWord').val().toLowerCase();
+    Game.setupWord(Game.wordToPlay);
     $('.modalOverlay').fadeOut(300);
     $('.modalBox').fadeOut(300);
   })
 
+  // Player 1 word generation
+  Game.$start.click(function(){
+    Game.wordToPlay = Game.wordsArr[Game.randomIndex].toLowerCase();
+    Game.setupWord(Game.wordToPlay);
+    $('.modalOverlay').fadeOut(300);
+    $('.modalBox').fadeOut(300);
+  })
+
+  // Play a letter
   Game.$guessSubmit.click(Game.playLetter);
 
   $(window).keydown(function(e){
-    console.log(e.keyCode);
     if (e.keyCode === 13) {
       Game.playLetter();
     }
